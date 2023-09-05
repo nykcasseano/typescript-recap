659f7c5a2c24ecf85618732ced46e88f17d74220
nykcasseano <nykcasseano13@gmail.com>
2023-09-05 11:42:23 -0300

    adding more files to the project

--- First.ts
+++ First.ts
@@ -1,2 +1,10 @@
-const a = 'Hello World!';
+var  a = 'Hello World!';
+var b = 5;
+var c = false;
+
+var someArray: string[] = [];
+someArray.push(a)
+someArray.push(b as any);
+
+
 console.log(a); // Hello World! 
